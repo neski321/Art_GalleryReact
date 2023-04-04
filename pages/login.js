c424a55212d72519b5841b64321dc02446a6ffc0
neskines <nmotieno@myseneca.ca>
2023-04-04 18:23:54 -0400

    init

--- pages/login.js
+++ pages/login.js
@@ -3,6 +3,67 @@ import { useState } from 'react';
 import { authenticateUser } from '@/lib/authenticate';
 import { useRouter } from 'next/router';
 import { getToken } from "@/lib/authenticate";
+import useSWR from 'swr';
+import { useAtom } from 'jotai';
+import { favouritesAtom, searchHistoryAtom } from '@/store';
+import { getFavourites, getHistory } from '@/lib/userData';
+
+export default function Login(props){
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [warning, setWarning] = useState('');
+  const router = useRouter();
+  const fetcher = (url) => fetch(url, { headers: { Authorization: `JWT ${getToken()}` }}).then((res) => res.json());
+  const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/favourites`, fetcher);
+  const [, setFavouritesList] = useAtom(favouritesAtom);
+  const [, setSearchHistory] = useAtom(searchHistoryAtom);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await authenticateUser(user, password);
+      await updateAtoms();
+      router.push('/favourites');
+    } catch (error) {
+      setWarning(error.message);
+    }
+  }
+
+  async function updateAtoms(){
+    setFavouritesList(await getFavourites());
+    setSearchHistory(await getHistory());
+  }
+
+  return (
+    <>
+      <Card bg="light">
+        <Card.Body><h2>Login</h2>Enter your login information below:</Card.Body>
+      </Card>
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>User:</Form.Label><Form.Control type="text" value={user} id="userName" name="userName" onChange={e => setUser(e.target.value)} />
+        </Form.Group>
+        <br />
+        <Form.Group>
+          <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        { warning && ( <><br /><Alert variant="danger">{warning}</Alert></> )}
+        <br />
+        <Button variant="primary" className="pull-right" type="submit">Login</Button>
+      </Form>
+    </>
+  );
+}
+
+
+
+/*import { Card, Form, Alert, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { authenticateUser } from '@/lib/authenticate';
+import { useRouter } from 'next/router';
+import { getToken } from "@/lib/authenticate";
+import useSWR from 'swr';
 
 export default function Login(props){
 
@@ -17,7 +78,7 @@ export default function Login(props){
     e.preventDefault();
     try {
       await authenticateUser(user, password);
-      router.push('/vehicles');
+      router.push('/favourites');
     } catch (err) {
       setWarning(err.message);
     }
@@ -43,4 +104,4 @@ export default function Login(props){
       </Form>
     </>
   );
-}
+}*/
